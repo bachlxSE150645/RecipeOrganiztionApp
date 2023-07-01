5c50ff361c22bbd63cda821a6ff794e1caad6be7
unknown <57053685+baolongt@users.noreply.github.com>
2023-07-01 17:23:38 +0700

    add relation virtual list

--- RecipeOrganizationApp/BusinessObjects/Account.cs
+++ RecipeOrganizationApp/BusinessObjects/Account.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BusinessObjects
@@ -21,6 +22,16 @@ namespace BusinessObjects
         [Required] public bool Status { get; set; }
 
         public Role Role { get; set; }
-        
+        [JsonIgnore]
+        public virtual ICollection<Meal> Meals { get; set; } = new List<Meal>();
+        [JsonIgnore]
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+        [JsonIgnore]
+        public virtual ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
+        [JsonIgnore]
+        public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+        [JsonIgnore]
+        public virtual ICollection<WishList> WishLists { get; set; } = new List<WishList>();
+
     }
 }
