51712c182bb6ae30a7be96f300de0f22cc57a68b
Mr-Lauchpad <chiennhse150667@fpt.edu.vn>
2023-07-03 23:10:51 +0700

    updateMealAPI

--- RecipeOrganizationApp/DataAccess/MealDAO.cs
+++ RecipeOrganizationApp/DataAccess/MealDAO.cs
@@ -1,4 +1,7 @@
 ﻿using BusinessObjects;
+using BusinessObjects.MapData;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Channels;
 
 namespace DataAccess
 {
@@ -16,7 +19,10 @@ namespace DataAccess
         {
             try
             {
-                return _context.Meals.ToList();
+                return this._context.Meals
+                    .Include(c => c.Account)
+                    .Include(c => c.Recipe)
+                    .ToList();
             }
             catch (Exception ex)
             {
@@ -25,33 +31,44 @@ namespace DataAccess
         }
 
         //Get Meal matches ID
-        public Meal GetMealsById(string id)
+        public Meal GetMealsById(Guid id)
         {
+            var meal = new Meal();
             try
             {
-                return _context.Meals.SingleOrDefault(x => x.MealID.Equals(id));
+
+                meal = _context.Meals
+                    .Include(c => c.Account)
+                    .Include(c => c.Recipe)
+                    .Where(x => x.MealID.Equals(id)).SingleOrDefault();
+
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                throw new Exception();
             }
+            return meal;
         }
 
         //Post new Meal
-        public void AddMeal(Meal meal)
+        public Meal AddMeal(MealData meal)
         {
             try
             {
                 var mealAdd = new Meal
                 {
+                    MealID = Guid.NewGuid(),
                     AccountID = meal.AccountID,
                     RecipeID = meal.RecipeID,
                     Price = meal.Price,
                     Description = meal.Description,
-                    Status = meal.Status,
+                    Status = "On Sale",
+                    Account = _context.Accounts.SingleOrDefault(c => c.AccountID == meal.AccountID),
+                    Recipe = _context.Recipes.SingleOrDefault(c => c.RecipeID == meal.RecipeID)
                 };
                 _context.Meals.Add(mealAdd);
                 _context.SaveChanges();
+                return mealAdd;
             }
             catch (Exception ex)
             {
@@ -60,16 +77,20 @@ namespace DataAccess
         }
 
         //Put existing Meal
-        public void UpdateMeal(Meal meal)
+        public Meal UpdateMeal(Guid mealID, decimal mealPrice, string mealDescription)
         {
             try
             {
-                var Meal = _context.Meals.SingleOrDefault(x => x.MealID == meal.MealID);
+                var Meal = _context.Meals.SingleOrDefault(x => x.MealID == mealID);
                 if (Meal != null)
                 {
-                    _context.Entry<Meal>(meal).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    Meal.Price = mealPrice;
+                    Meal.Description = mealDescription;
+
+                    _context.Entry<Meal>(Meal).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
                     _context.SaveChanges();
                 }
+                return Meal;
             }
             catch (Exception ex)
             {
@@ -78,16 +99,19 @@ namespace DataAccess
         }
 
         //Delete existing Meal
-        public void DeleteMeal(Meal meal)
+        public bool DeleteMeal(Guid mealID)
         {
             try
             {
-                var Meal = _context.Meals.SingleOrDefault(x => x.MealID.Equals(meal.MealID));
+                bool check = false;
+                var Meal = _context.Meals.SingleOrDefault(x => x.MealID.Equals(mealID));
                 if (Meal != null)
                 {
                     _context.Meals.Remove(Meal);
                     _context.SaveChanges();
+                    check = true;
                 }
+                return check;
             }
             catch (Exception ex)
             {
